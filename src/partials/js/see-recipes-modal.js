4f318251d95071a3d168a8af510f3a43407e8447
feyza yarcı <yarcifeyza@gmail.com>
2025-02-04 15:56:24 +0300

    recipes html css js eklendi

---
+++ src/partials/js/see-recipes-modal.js
@@ -0,0 +1,36 @@
+const seeRecipesBtn = document.querySelector('.card-section-see-recipe-btn');
+const closeBtn = document.querySelector('.see-recipes-close-btn');
+const seeRecipesModal = document.querySelector('#see-recipes-modal');
+const modalName = document.querySelector('#see-recipes-modal-name');
+const modalTags = document.querySelector('#see-recipes-modal-tags');
+const modalRating = document.querySelector('#see-recipes-modal-rating');
+const modalRatingStars = document.querySelector('#see-recipes-modal-rating-stars');
+const modalTime = document.querySelector('#see-recipes-modal-time');
+const modalVideoFrame = document.querySelector('#see-recipes-modal-video-frame');
+const modalMaterials = document.querySelector('#see-recipes-modal-material');
+const modalInstructions = document.querySelector('#see-recipes-modal-instructions');
+
+
+seeRecipesBtn.addEventListener('click', async function () {
+  seeRecipesModal.style.display = 'block';
+});
+closeBtn.addEventListener('click', function () {
+  seeRecipesModal.style.display = 'none';
+});
+async function fetchRecipes(recipes) {
+  const apiKeyRecipesUrl = ``;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
