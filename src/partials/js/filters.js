a8eac55ad5a9ed3b20159b7ceb7af0ec5a0b1864
Osman <157528072+osmnr@users.noreply.github.com>
2025-02-08 22:06:06 +0300

    updated
    updated

--- src/partials/js/filters.js
+++ src/partials/js/filters.js
@@ -4,12 +4,9 @@ const allIngredientsURL = "https://tasty-treats-backend.p.goit.global/api/ingred
 const response = await axios.get(allIngredientsURL);
 const ingredientUL = document.getElementById("ingredient-options");
 ingredientUL.style.display = "flex";
-let listOfAllIngredients = [];
 for (let i=0 ; i<response.data.length ; i++){
-    listOfAllIngredients.push(response.data[i].name)
     const markupLi = `<li class=filter-hidden-dropdown-li-ingredient>${response.data[i].name}</li>`
     ingredientUL.insertAdjacentHTML("beforeend", markupLi);
 }
-console.log("bütün içeriklerin listesi:", listOfAllIngredients);
 
 
