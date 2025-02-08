5f835fb709684ed30f1f30fff346df8724cb374c
feyza yarcı <yarcifeyza@gmail.com>
2025-02-08 20:10:44 +0300

    see-recipes

--- src/partials/js/see-recipes-modal.js
+++ src/partials/js/see-recipes-modal.js
@@ -1,17 +1,32 @@
-const seeRecipesBtn = document.querySelector('.card-section-see-recipe-btn');
-const closeBtn = document.querySelector('.see-recipes-close-btn');
-const seeRecipesModal = document.querySelector('#see-recipes-modal');
-const modalName = document.querySelector('#see-recipes-modal-name');
-const modalTags = document.querySelector('#see-recipes-modal-tags');
-const modalRating = document.querySelector('#see-recipes-modal-rating');
-const modalRatingStars = document.querySelector('#see-recipes-modal-rating-stars');
-const modalTime = document.querySelector('#see-recipes-modal-time');
-const modalVideoFrame = document.querySelector('#see-recipes-modal-video-frame');
-const modalMaterials = document.querySelector('#see-recipes-modal-material');
-const modalInstructions = document.querySelector('#see-recipes-modal-instructions');
-// seeRecipesBtn.addEventListener('click', async function () {
-//     seeRecipesModal.style.display = 'flex';
-// });
-// closeBtn.addEventListener('click', function () {
-//     seeRecipesModal.style.display = 'none';
-// });
+const popularRecipesImg = document.querySelectorAll(".pop-recipe-img");
+
+popularRecipesImg.forEach((img) => {
+  img.addEventListener('click', handleClickSeeRecipesButton);
+});
+
+async function handleClickSeeRecipesButton(event) {
+  console.log("Tıklanan tarifin id'si:", event.target.id);
+  let getRecipeByIdURL = `https://tasty-treats-backend.p.goit.global/api/recipes/${event.target.id}`;
+  const responseRecipe = await axios.get(getRecipeByIdURL);
+  const recipeData = responseRecipe.data;
+  console.log("Tıklanan tarife ait modal'da kullanılacak data:", recipeData);
+
+  document.querySelector("#see-recipes-modal-name").innerText = recipeData.title;
+  document.querySelector("#see-recipes-modal-video").innerHTML = `
+    <img src="${recipeData.preview}" alt="${recipeData.title}" />
+    <!-- <iframe width="550" height="250" src="${recipeData.videoUrl}" frameborder="0" allowfullscreen></iframe> -->
+  `;
+  document.querySelector("#see-recipes-modal-tags").innerText = recipeData.category;
+  document.querySelector("#see-recipes-modal-rating").innerText = recipeData.rating;
+  document.querySelector("#see-recipes-modal-time").innerText = `${recipeData.time} min`;
+  document.querySelector("#see-recipes-modal-material").innerText = recipeData.ingredients.join(', ');
+  document.querySelector("#see-recipes-modal-instructions").innerText = recipeData.instructions;
+
+  const modal = document.querySelector('#see-recipes-modal');
+  modal.style.display = 'flex';
+
+  const closeButton = document.querySelector('.see-recipes-close-btn');
+  closeButton.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
+}
