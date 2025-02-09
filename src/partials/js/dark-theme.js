e102c57122514170035d933f560be36f5431f89e
Yelenay Uçar <yelenayucr@gmail.com>
2025-02-09 23:15:56 +0300

    Update dark-theme.js

--- src/partials/js/dark-theme.js
+++ src/partials/js/dark-theme.js
@@ -3,10 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (localStorage.getItem("theme") === "dark") {
         document.body.classList.add("dark-mode");
-        themeToggle.checked = true; // Buton işaretli başlar
+        themeToggle.checked = true; 
     } else {
         document.body.classList.remove("dark-mode");
-        themeToggle.checked = false; // Varsayılan olarak light mode
+        themeToggle.checked = false; 
     }
 
     themeToggle.addEventListener("change", () => {
